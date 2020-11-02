6573712a398d60371a7b9d6a0b577602423ae396
Graham Walters <git@grahamwalters.me>
2020-11-02 12:25:09 -0500

    Create librdkafka@1.3.0.rb

---
+++ librdkafka@1.3.0.rb
@@ -0,0 +1,34 @@
+class LibrdkafkaAT130 < Formula
+  desc "The Apache Kafka C/C++ library"
+  homepage "https://github.com/edenhill/librdkafka"
+  url "https://github.com/edenhill/librdkafka/archive/v1.3.0.tar.gz"
+  sha256 "465cab533ebc5b9ca8d97c90ab69e0093460665ebaf38623209cf343653c76d2"
+  head "https://github.com/edenhill/librdkafka.git"
+
+  depends_on "pkg-config" => :build
+  depends_on "lz4"
+  depends_on "lzlib"
+  depends_on "openssl@1.1"
+  depends_on "zstd"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <librdkafka/rdkafka.h>
+
+      int main (int argc, char **argv)
+      {
+        int partition = RD_KAFKA_PARTITION_UA; /* random */
+        int version = rd_kafka_version();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lrdkafka", "-lz", "-lpthread", "-o", "test"
+    system "./test"
+  end
+end
